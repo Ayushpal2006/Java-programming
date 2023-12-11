adbddc7ad26603e888745efb2f80846d47bfc3f8
“Ayush <ayushpalworkplace2006@gmail.com>
2023-12-11 23:13:57 +0530

    added

---
+++ patterns/pattern31.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class pattern31 {
+   static void main(String[] args){
+    pat31(5);
+   } 
+
+   static void pat31(int n){
+
+    n=2*n;
+    for(int row=0;row<=n;row++){
+        for(int col=0;col<=n;col++){
+             int AtEveryValue = 0;
+        System.out.println(AtEveryValue+ " ");
+
+        }
+        System.out.println();
+    }
+
+
+   }
+}
