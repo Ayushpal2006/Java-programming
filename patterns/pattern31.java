47e3623a74ff39d27f2d525f35094d43d2898078
“Ayush <ayushpalworkplace2006@gmail.com>
2023-12-11 23:37:13 +0530

    added by ayush

--- patterns/pattern31.java
+++ patterns/pattern31.java
@@ -8,7 +8,7 @@ public class pattern31 {
 static void pat31(int n){
 
     int originalN = n;
-        n = 2 * n;
+        n = 2 * n; 
         for (int row = 0; row <= n; row++) {
             for (int col = 0; col <= n; col++) {
                 int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
